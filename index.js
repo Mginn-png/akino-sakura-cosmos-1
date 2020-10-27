bd3a4ee1c5c075fbaf419cd268ed17ef43827382
Mginn-png <73435942+Mginn-png@users.noreply.github.com>
2020-10-27 22:44:10 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const axios = require("axios")
+const urls = ["https://sakura-akino-cosmos.glitch.me"]
+setInterval(function() {
+            urls.forEach(url => {
+            axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
+        })
+    }, 60 * 1000);
